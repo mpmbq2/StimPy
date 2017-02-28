73d86c388d1c7aa2e97e23235b23971290d5d51d
mpmbq2 <mpmbq2@gmail.com>
2017-02-28 18:21:34 -0500

    reverse __init__ and mini imports

--- __init__.py
+++ __init__.py
@@ -1,1 +1,20 @@
 from . import *
+
+
+class DataSet():
+
+    def __init__(self):
+        pass
+
+    def _register(self):
+        pass
+
+    class AbfRecording():
+        def __init__(self):
+            pass
+
+        def _read(self):
+            pass
+
+        def process(self):
+            pass
