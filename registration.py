dfc7d6cd31c4a225a95d32be36dcee2ab4f286bd
Matt McCabe <mpmbq2@gmail.com>
2017-03-01 07:37:05 -0500

    Create registration.py
    added commented registration file to describe the workflow for an eventual registration process

---
+++ registration.py
@@ -0,0 +1,24 @@
+import numpy as np
+import stimfit as stf
+import stfio
+import pandas as pd
+
+def main(path):
+    '''Guided registration of stimulation artifact and PSC duration for automated analysis'''
+    
+    # Step 1: load metadata from DataSet object in <path>
+    # Step 2: Find the 'baseline' file for each recorded pair
+    # For Channel 1
+        # Step 3: Prompt user to set latency cursors are start and end of artefact
+            # raw_input('Please set latency cursors to encomapse the stimulation artefact. Type "c" to continue.')
+        # Step 4: Prompt user to set peak cursors over the psc, and fit cursors from the peak to the baseline
+        # Step 5: Store the [start] and [end] points of the artifact in an 'artifact' dict
+        # Step 6: Store the [fit] values of the decay
+        # Step 7: Repeat steps 3-6 for the stimulation in Channel 2 (remember to save the values into the other neuron)
+    # For Channel 2
+        # Step 8: Prompt user to set latency cursors are start and end of artefact
+            # raw_input('Please set latency cursors to encomapse the stimulation artefact. Type "c" to continue.')
+        # Step 9: Prompt user to set peak cursors over the psc, and fit cursors from the peak to the baseline
+        # Step 10: Store the [start] and [end] points of the artefact in an 'artefact' dict
+        # Step 11: Store the [fit] values of the decay
+        # Step 12: Repeat steps 8-11 for the stimulation in Channel 1 (remember to save the values into the other neuron)
