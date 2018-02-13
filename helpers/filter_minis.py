21fb5fe1c51ab27c07a2daaf21e1bcb1fb179fa4
mpmbq2 <mpmbq2@gmail.com>
2018-02-13 17:28:31 -0500

    Improved mini fitting by breaking trace into phases

--- helpers/filter_minis.py
+++ helpers/filter_minis.py
@@ -1,37 +1,16 @@
-def filter_events(traces):
-    
-    keepers = np.ones(traces.shape[0])
-    def template(time, norm, rise, decay):
-        out = norm * (np.exp(-time/rise) - np.exp(-time/decay))
-        return out
-    
-    for idx, trace in enumerate(traces):
-        base = np.median(trace)
-        trace = trace-base
-        trace = trace[105:200]
-        time = np.arange(0, len(trace))
-        p0 = np.array([20, 8, 20])
-        params, _ = curve_fit(template, time, trace, p0=p0)
-        fit = template(time, params[0], params[1], params[2])
-        base = np.median(trace)
-        peak = np.min(fit)
-        over_half = fit < peak/2
-        halfs = [i for i, x in enumerate(np.diff(over_half)) if x]
-        print(peak)
-        print(halfs)
-    return True
-  
 
 def fit_trace(trace):
     
     def template(time, norm, rise, decay, delay, end):
+        delay_phase = np.zeros(delay)
+        event_phase = np.arange(delay-delay, end-delay)
+        end_phase = np.zeros(len(time) - end)
+        time = np.concatenate((delay_phase, event_phase, end_phase), axis=0)
         out = norm * (np.exp(-time/rise) - np.exp(-time/decay))
-        out[:delay] = 0
-        out[end:] = 0
         return out
 
     time = np.arange(0, len(trace))
-    p0 = np.array([20, 8, 20, 50, 300])
+    p0 = np.array([20, 8, 20, 100, 300])
     
     def residuals(p0, trace, time):
         return trace - template(time, p0[0], p0[1], p0[2], p0[3], p0[4])
